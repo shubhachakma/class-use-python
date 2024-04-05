ab4c0491606d31cbec3b9dbd48553533dc70ff61
shubhachakma <shubhachakma600@gmail.com>
2024-04-06 00:05:21 +0600

    triangle area added

--- test.py
+++ test.py
@@ -11,3 +11,25 @@ class Max3Num:
 #b = int(input("B="))
 #c = int(input("C="))
 #max3Num = Max3Num(a, b, c)
+
+
+
+import math
+class Trianglearea:
+    def __init__(self, a, b, c):
+        import math
+        if (a+b)>c and (b+c)>a and (a+c)>b:
+            s=(a+b+c)/2
+            area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+            print("Triangle Area",area)
+        else:
+            print("Triangle is not possible")
+a = float(input("Enter first Arm="))
+b = float(input("Enter second Arm="))
+c = float(input("Enter third Arm="))
+tarea = Trianglearea(a,b,c)
+
+
+#Enter first Arm=4
+#Enter first Arm=3
+#Enter first Arm=2
